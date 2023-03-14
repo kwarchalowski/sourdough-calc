a4c91b3c23de9397bd87f0e506d1d8cf13299d8d
kwarchalowski <karol@warchalowski.pl>
2023-03-14 14:28:14 +0100

    Ingredients sum

--- src/app/total-ingredients/total-ingredients.component.ts
+++ src/app/total-ingredients/total-ingredients.component.ts
@@ -7,78 +7,43 @@ import { Ingredients } from '../ingredients';
   styleUrls: ['./total-ingredients.component.scss']
 })
 export class TotalIngredientsComponent implements OnChanges {
-  
+
   @Input() doughWeight = 0;
   @Input() scale = 0;
 
-  
+
   model = new Ingredients(100, 70, 2, 0, 0, 0, 0, 0);
 
-  strongWhiteFlourWeight = 0;
-  waterWeight = 0;
-  flourType2Weight = 0;
-  flourType3Weight = 0;
-  saltWeight = 0;
-  inclusion1Weight = 0;
-  inclusion2Weight = 0;
-  inclusion3Weight = 0;
+  //* init
+  strongWhiteFlourWeight: number = 0;
+  waterWeight: number = 0;
+  flourType2Weight: number = 0;
+  flourType3Weight: number = 0;
+  saltWeight: number = 0;
+  inclusion1Weight: number = 0;
+  inclusion2Weight: number = 0;
+  inclusion3Weight: number = 0;
+  totalWeight: number = 0;
 
   ngOnInit() {
     this.recalculateWeights();
   }
-  
+
   ngOnChanges(changes: SimpleChanges) {
     //this.recalculateWeights();
   }
 
-  //* that's for map
-  ingredientsPairs = [
-    {
-      bakers: this.model.strongWhiteFlourBakers,
-      weight: this.strongWhiteFlourWeight
-    },
-    {
-      bakers: this.model.strongWhiteFlourBakers,
-      weight: this.strongWhiteFlourWeight
-    },
-    {
-      bakers: this.model.strongWhiteFlourBakers,
-      weight: this.strongWhiteFlourWeight
-    },
-    {
-      bakers: this.model.strongWhiteFlourBakers,
-      weight: this.strongWhiteFlourWeight
-    },
-    {
-      bakers: this.model.strongWhiteFlourBakers,
-      weight: this.strongWhiteFlourWeight
-    },
-    {
-      bakers: this.model.strongWhiteFlourBakers,
-      weight: this.strongWhiteFlourWeight
-    },
-    {
-      bakers: this.model.strongWhiteFlourBakers,
-      weight: this.strongWhiteFlourWeight
-    },
-    {
-      bakers: this.model.strongWhiteFlourBakers,
-      weight: this.strongWhiteFlourWeight
-    }
-  ]
 
-  // TODO: find out how to sum all of the ingredients
-  totalBakers: number = this.ingredientsPairs.reduce((acc, ingredient) => acc + ingredient.bakers, 0);
-  totalWeight: number = this.ingredientsPairs.reduce((acc, ingredient) => acc + ingredient.weight, 0);
-  
   recalculateWeights(): void {
+
+
 
     // TODO: think about map.reduce
     // this.ingredientsPairs.forEach(ingredient => console.log(ingredient.weight));
 
     // this.ingredientsPairs.map(ingredient => ingredient.weight = parseFloat(((this.doughWeight / (this.totalBakers) * (ingredient.bakers)) * this.scale).toFixed(1)));
     // console.log('after: ' + this.ingredientsPairs);
-    
+
     this.model.strongWhiteFlourBakers = 100 - this.model.flourType2Bakers - this.model.flourType3Bakers;
 
     this.strongWhiteFlourWeight = parseFloat(((this.doughWeight / (this.model.totalBakers()) * (this.model.strongWhiteFlourBakers)) * this.scale).toFixed(1));
@@ -89,6 +54,43 @@ export class TotalIngredientsComponent implements OnChanges {
     this.inclusion1Weight = parseFloat(((this.doughWeight / (this.model.totalBakers()) * (this.model.inclusion1Bakers)) * this.scale).toFixed(1));
     this.inclusion2Weight = parseFloat(((this.doughWeight / (this.model.totalBakers()) * (this.model.inclusion2Bakers)) * this.scale).toFixed(1));
     this.inclusion3Weight = parseFloat(((this.doughWeight / (this.model.totalBakers()) * (this.model.inclusion3Bakers)) * this.scale).toFixed(1));
+
+    var ingredientsPairs = [
+      {
+        bakers: this.model.strongWhiteFlourBakers,
+        weight: this.strongWhiteFlourWeight
+      },
+      {
+        bakers: this.model.waterBakers,
+        weight: this.waterWeight
+      },
+      {
+        bakers: this.model.flourType2Bakers,
+        weight: this.flourType2Weight
+      },
+      {
+        bakers: this.model.flourType3Bakers,
+        weight: this.flourType3Weight
+      },
+      {
+        bakers: this.model.saltBakers,
+        weight: this.saltWeight
+      },
+      {
+        bakers: this.model.inclusion1Bakers,
+        weight: this.inclusion1Weight
+      },
+      {
+        bakers: this.model.inclusion2Bakers,
+        weight: this.inclusion2Weight
+      },
+      {
+        bakers: this.model.inclusion3Bakers,
+        weight: this.inclusion3Weight
+      }
+    ]
+
+    this.totalWeight = ingredientsPairs.reduce((acc, ingredient) => acc + ingredient.weight, 0);
   };
 
 }
