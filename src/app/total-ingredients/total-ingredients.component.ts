fbd9c698b3565eaae1b2b2efed38a7d5557f263e
kwarchalowski <karol@warchalowski.pl>
2023-03-14 12:36:44 +0100

    Total ingredients in a table

--- src/app/total-ingredients/total-ingredients.component.ts
+++ src/app/total-ingredients/total-ingredients.component.ts
@@ -28,20 +28,67 @@ export class TotalIngredientsComponent implements OnChanges {
   }
   
   ngOnChanges(changes: SimpleChanges) {
-    this.recalculateWeights();
+    //this.recalculateWeights();
   }
 
+  //* that's for map
+  ingredientsPairs = [
+    {
+      bakers: this.model.strongWhiteFlourBakers,
+      weight: this.strongWhiteFlourWeight
+    },
+    {
+      bakers: this.model.strongWhiteFlourBakers,
+      weight: this.strongWhiteFlourWeight
+    },
+    {
+      bakers: this.model.strongWhiteFlourBakers,
+      weight: this.strongWhiteFlourWeight
+    },
+    {
+      bakers: this.model.strongWhiteFlourBakers,
+      weight: this.strongWhiteFlourWeight
+    },
+    {
+      bakers: this.model.strongWhiteFlourBakers,
+      weight: this.strongWhiteFlourWeight
+    },
+    {
+      bakers: this.model.strongWhiteFlourBakers,
+      weight: this.strongWhiteFlourWeight
+    },
+    {
+      bakers: this.model.strongWhiteFlourBakers,
+      weight: this.strongWhiteFlourWeight
+    },
+    {
+      bakers: this.model.strongWhiteFlourBakers,
+      weight: this.strongWhiteFlourWeight
+    }
+  ]
+
+  // TODO: find out how to sum all of the ingredients
+  totalBakers: number = this.ingredientsPairs.reduce((acc, ingredient) => acc + ingredient.bakers, 0);
+  totalWeight: number = this.ingredientsPairs.reduce((acc, ingredient) => acc + ingredient.weight, 0);
+  
   recalculateWeights(): void {
-    // TODO: they are not numbers?
+
+    // TODO: think about map.reduce
+    // this.ingredientsPairs.forEach(ingredient => console.log(ingredient.weight));
+
+    // this.ingredientsPairs.map(ingredient => ingredient.weight = parseFloat(((this.doughWeight / (this.totalBakers) * (ingredient.bakers)) * this.scale).toFixed(1)));
+    // console.log('after: ' + this.ingredientsPairs);
+    
     this.model.strongWhiteFlourBakers = 100 - this.model.flourType2Bakers - this.model.flourType3Bakers;
+
     this.strongWhiteFlourWeight = parseFloat(((this.doughWeight / (this.model.totalBakers()) * (this.model.strongWhiteFlourBakers)) * this.scale).toFixed(1));
-    this.waterWeight = (this.doughWeight / (this.model.totalBakers()) * (this.model.waterBakers)) * this.scale;
-    this.flourType2Weight = (this.doughWeight / (this.model.totalBakers()) * (this.model.flourType2Bakers)) * this.scale;
-    this.flourType3Weight = (this.doughWeight / (this.model.totalBakers()) * (this.model.flourType3Bakers)) * this.scale;
-    this.saltWeight = (this.doughWeight / (this.model.totalBakers()) * (this.model.saltBakers)) * this.scale;
-    this.inclusion1Weight = (this.doughWeight / (this.model.totalBakers()) * (this.model.inclusion1Bakers)) * this.scale;
-    this.inclusion2Weight = (this.doughWeight / (this.model.totalBakers()) * (this.model.inclusion2Bakers)) * this.scale;
-    this.inclusion3Weight = (this.doughWeight / (this.model.totalBakers()) * (this.model.inclusion3Bakers)) * this.scale;
+    this.waterWeight = parseFloat(((this.doughWeight / (this.model.totalBakers()) * (this.model.waterBakers)) * this.scale).toFixed(1));
+    this.flourType2Weight = parseFloat(((this.doughWeight / (this.model.totalBakers()) * (this.model.flourType2Bakers)) * this.scale).toFixed(1));
+    this.flourType3Weight = parseFloat(((this.doughWeight / (this.model.totalBakers()) * (this.model.flourType3Bakers)) * this.scale).toFixed(1));
+    this.saltWeight = parseFloat(((this.doughWeight / (this.model.totalBakers()) * (this.model.saltBakers)) * this.scale).toFixed(1));
+    this.inclusion1Weight = parseFloat(((this.doughWeight / (this.model.totalBakers()) * (this.model.inclusion1Bakers)) * this.scale).toFixed(1));
+    this.inclusion2Weight = parseFloat(((this.doughWeight / (this.model.totalBakers()) * (this.model.inclusion2Bakers)) * this.scale).toFixed(1));
+    this.inclusion3Weight = parseFloat(((this.doughWeight / (this.model.totalBakers()) * (this.model.inclusion3Bakers)) * this.scale).toFixed(1));
   };
 
 }
